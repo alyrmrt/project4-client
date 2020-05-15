6b68055ed6e1f71d120aadd4282a8c4476a12250
alyrtmrt <alayurtmm@gmail.com>
2020-05-14 21:38:41 -0400

    fixed bugs on config

--- src/apiConfig.js
+++ src/apiConfig.js
@@ -1,6 +1,6 @@
 let apiUrl
 const apiUrls = {
-  production: 'https://bookfindermurat.herokuapp.com/',
+  production: 'https://bookfindermurat.herokuapp.com',
   development: 'http://localhost:4741'
 }
 
